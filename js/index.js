c164c5bd925aec324fad5e24ff23eef4bfd063c6
GAldereteHero <herogonzalo@gmail.com>
2022-07-15 16:29:10 -0300

    Modificacion del menu

--- js/index.js
+++ js/index.js
@@ -2,16 +2,12 @@
 class Menu{
     constructor(){
         this.hamburguesas = [];
-        this.pizzas = [];
         this.birras = [];
         this.arrayMenu = []; 
     }
 
     cargarHamburguesas(obj){
         this.hamburguesas.push(obj);
-    }
-    cargarPizzas(obj){
-        this.pizzas.push(obj);
     }
     cargarBirra(obj){
         this.birras.push(obj);
@@ -22,9 +18,6 @@ class Menu{
     
         mostrar.push(`----Hamburguesas------------`);
         this.hamburguesas.forEach((item) => mostrar.push(`$${item.precio} ${item.nombre}: ${item.ingred1}, ${item.ingred2}, ${item.ingred3}, ${item.aderezo}.`));
-    
-        mostrar.push(`----Pizzas------------------`);
-        this.pizzas.forEach((item) => mostrar.push(`$${item.precio} ${item.nombre}: ${item.ingred1}, ${item.ingred2}, ${item.ingred3}.`));
         
         mostrar.push(`----Birras------------------`);
         this.birras.forEach((item) =>  mostrar.push(`$${item.precio} ${item.nombre}: Grad. Alcoh: ${item.gradAlco}`));
@@ -35,7 +28,6 @@ class Menu{
     cargarArrayMenu(){
         // Este metodo permite cargar en un array todos los elementos del menu
         this.hamburguesas.forEach( (el) => this.arrayMenu.push(el));
-        this.pizzas.forEach( (el) => this.arrayMenu.push(el));
         this.birras.forEach( (el) => this.arrayMenu.push(el));
     }
 }
@@ -81,16 +73,6 @@ class Hamburguesa{
     }
 }
 
-class Pizza{
-    constructor(nombre, ingred1, ingred2, ingred3, precio){
-        this.nombre = nombre;
-        this.ingred1 = ingred1;
-        this.ingred2 = ingred2;
-        this.ingred3 = ingred3;
-        this.precio = precio;
-    }
-}
-
 class Birra{
     constructor(nombre, gradAlco, precio){
         this.nombre = nombre;
@@ -112,28 +94,24 @@ function IsValidItem(item,cantidad){
     }
 }
 
+const title = (word) => word[0].toUpperCase() + word.substring(1).toLowerCase();
+
 /*---- Instancias ----*/
 
 const miMenu = new Menu();
+const miPedido = new Pedido();
 
 miMenu.cargarHamburguesas(new Hamburguesa("Parisina", "Hongos", "Queso Azul", "Cebolla caramelizada", "Barbacoa", 800 ));
 miMenu.cargarHamburguesas(new Hamburguesa("Mexicanita", "Palta", "Tomates confitados", "Pimientos asados", "Barbacoa", 850 ));
 miMenu.cargarHamburguesas(new Hamburguesa("Argenta", "Rucula", "Tomates", "Huevo", "Alioli", 900 ));
-
-miMenu.cargarPizzas(new Pizza("Especial", "Jamon", "Pimiento", "Aceitunas", 950 ));
-miMenu.cargarPizzas(new Pizza("Agridulce", "Jamon crudo", "Cebollas caramelizadas", "Queso azul", 1000 ));
-miMenu.cargarPizzas(new Pizza("De la casa", "Ternera", "Huevo", "Pimiento", 1100 ));
+miMenu.cargarHamburguesas(new Hamburguesa("Yanky", "Panceta", "Aros de cebolla", "Cheddar", "Alioli", 900 ));
 
 miMenu.cargarBirra(new Birra("Honey","5%", 200));
 miMenu.cargarBirra(new Birra("Trigo","6%", 250));
 miMenu.cargarBirra(new Birra("Scottish","7%", 300));
-
-const miPedido = new Pedido();
-
-const title = (word) => word[0].toUpperCase() + word.substring(1).toLowerCase();
+miMenu.cargarBirra(new Birra("Lemon","8%", 350));
 
 miMenu.cargarArrayMenu();
-
 miMenu.mostrarMenu();
 
 let it = 'y';

201c5e4ae91bccca97d1a5a98e02740d6e43e9ce
GAldereteHero <herogonzalo@gmail.com>
2022-08-11 15:27:06 -0300

    Se agrega fetch desde Json

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 import { Menu, Pedido, Hamburguesa, Birra, title } from './metaData.js'
 
+document.addEventListener('DOMContentLoaded', e => {
+    traerItems();
+    recomendacionesChef();
+    
+    // Inicializamos el pedido. 
+    JSON.parse(localStorage.getItem('items')) ? miPedido.renderPedido() : null; 
+})
+
 const arraySaludos = ["¡Las mejores hamburguesas","de Mr. Burgui",'para compartir con amigos!'];
-
 const hamburguesas = ["Alemana","Mexicana","Francesa","Americana"];
-
-/*---- Instancias ----*/
 
 console.log(...arraySaludos);
 
@@ -16,22 +21,16 @@ function recomendacionesChef(){
 const miMenu = new Menu();
 const miPedido = new Pedido();
 
-miMenu.cargarItem(new Hamburguesa('h1', "Alemana", "Cebolla rebozada", "Bacon", "Queso cheddar", "Barbacoa", 800, 1));
-miMenu.cargarItem(new Hamburguesa('h2', "Mexicana", "Guacamole", "Chili", "Queso cheddar", "Salsa ahumada", 850, 1));
-miMenu.cargarItem(new Hamburguesa('h3', "Francesa", "Lechuga", "Cebolla caramelizada", "Queso de cabra", "Salsa de miel", 900, 1));
-miMenu.cargarItem(new Hamburguesa('h4', "Americana", "Bacon", "Queso cheddar", "Huevo", "Salsa ahumada", 950, 1));
+const traerItems = async () => {
+    const resp = await fetch(`../json/data.json`)
+    const data = await resp.json()
+   
+    data.forEach((producto) => {
+        miMenu.cargarItem(producto);
+    })
 
-miMenu.cargarItem(new Birra('b1', "Honey", "5%", 200, 1));
-miMenu.cargarItem(new Birra('b2', "Weisse", "6%", 250, 1));
-miMenu.cargarItem(new Birra('b3', "Scottish", "7%", 300, 1));
-miMenu.cargarItem(new Birra('b4', "Red Ale", "8%", 350, 1));
-
-miMenu.renderMenu(miPedido);
-
-recomendacionesChef();
-
-// Inicializamos el pedido. 
-JSON.parse(localStorage.getItem('items')) ? miPedido.renderPedido() : localStorage.setItem("items", JSON.stringify([]));
+    miMenu.renderMenu(miPedido);
+}
 
 const btVerMiPedido = document.getElementById('verMiPedido');
 btVerMiPedido.addEventListener('click', () => {
@@ -55,7 +54,7 @@ btEnviarMiPedido.addEventListener('click', () => {
     if(JSON.parse(localStorage.getItem('items'))){
         miPedido.borrarPedido();
         Toastify({
-            text: "Tu pedido llego a la cocina!",
+            text: "¡Tu pedido llegó a la cocina!",
             style: {background: '#48C78E',},
             gravity: "bottom", // `top` or `bottom`
             position: "right", // `left`, `center` or `right`
